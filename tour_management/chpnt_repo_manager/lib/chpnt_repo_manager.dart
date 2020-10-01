06fa1e7e8507e4a5700681281be1eb442ef33b77
Keith111216 <keithwong2205@gmail.com>
2020-10-01 19:23:30 +0700

    adding checkpoint repository for manager to add and edit

--- tour_management/chpnt_repo_manager/lib/chpnt_repo_manager.dart
+++ tour_management/chpnt_repo_manager/lib/chpnt_repo_manager.dart
@@ -1,1 +1,6 @@
 library chpnt_repo_manager;
+
+//Barrel export checkpoint repository for manager
+export './source/chpoint_entity.dart';
+export './source/chpoint_model.dart';
+export './source/firebase_chpoint_service.dart';
