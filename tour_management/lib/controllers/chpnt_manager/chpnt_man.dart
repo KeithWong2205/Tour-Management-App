7b373967f56937bffdbbbbfae94282059e5d0e25
Keith111216 <keithwong2205@gmail.com>
2020-10-02 10:12:47 +0700

    checkpoint manager bloc

---
+++ tour_management/lib/controllers/chpnt_manager/chpnt_man.dart
@@ -0,0 +1,4 @@
+export './chpnt_mbloc.dart';
+export './chpnt_mevent.dart';
+export 'chpnt_mstate.dart';
+//Barrel export checkpoint manager bloc
---
+++ tour_management/lib/controllers/chpnt_manager/chpnt_mbloc.dart
@@ -0,0 +1,68 @@
+import 'dart:async';
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+import 'package:chpnt_repo_manager/chpnt_repo_manager.dart';
+import 'package:tour_management/controllers/chpnt_manager/chpnt_man.dart';
+
+//Checkpoint business bloc
+class CheckpointManBloc extends Bloc<CheckpointManEvent, CheckpointManState> {
+  final FirebaseCheckpointService firebaseCheckpointService;
+  StreamSubscription _checkpointSubscription;
+  CheckpointManBloc({@required this.firebaseCheckpointService})
+      : assert(firebaseCheckpointService != null),
+        super(CheckpointManLoadin());
+
+//Using stream to map events to states
+  @override
+  Stream<CheckpointManState> mapEventToState(CheckpointManEvent event) async* {
+    if (event is CheckpointManLoaded) {
+      yield* mapCheckpointManLoaded();
+    }
+    if (event is CheckpointManAdded) {
+      yield* mapCheckpointManAdded(event);
+    }
+    if (event is CheckpointManUpdated) {
+      yield* mapCheckpointManUpdated(event);
+    }
+    if (event is CheckpointManDelete) {
+      yield* mapCheckpointManDelete(event);
+    }
+    if (event is CheckpointListManUpdate) {
+      yield* mapCheckpointListManUpdate(event);
+    }
+  }
+
+  Stream<CheckpointManState> mapCheckpointManLoaded() async* {
+    _checkpointSubscription?.cancel();
+    _checkpointSubscription = firebaseCheckpointService
+        .checkpoints()
+        .listen((checkpoints) => add(CheckpointListManUpdate(checkpoints)));
+  }
+
+  Stream<CheckpointManState> mapCheckpointManAdded(
+      CheckpointManAdded event) async* {
+    firebaseCheckpointService.addNewCheckpoint(event.checkpoint);
+  }
+
+  Stream<CheckpointManState> mapCheckpointManUpdated(
+      CheckpointManUpdated event) async* {
+    firebaseCheckpointService.updateCheckpoint(event.updated);
+  }
+
+  Stream<CheckpointManState> mapCheckpointManDelete(
+      CheckpointManDelete event) async* {
+    firebaseCheckpointService.deleteCheckpoint(event.deleted);
+  }
+
+  Stream<CheckpointManState> mapCheckpointListManUpdate(
+      CheckpointListManUpdate event) async* {
+    yield CheckpointManLoadSuccess(event.updatedList);
+
+    @override
+    // ignore: unused_element
+    Future<void> close() {
+      _checkpointSubscription?.cancel();
+      return super.close();
+    }
+  }
+}
