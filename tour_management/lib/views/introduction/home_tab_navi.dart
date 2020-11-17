eafaf6e2be29c8e37dd6039cd260136cad4ecbe5
Keith111216 <keithwong2205@gmail.com>
2020-11-17 18:51:58 +0700

    Update on welcome page

--- tour_management/lib/views/introduction/home_tab_navi.dart
+++ tour_management/lib/views/introduction/home_tab_navi.dart
@@ -20,18 +20,31 @@ class _HomeTabNaviState extends State<HomeTabNavi> {
           alignment: Alignment.center,
           child: ListView(
             children: [
-              SizedBox(height: 20),
-              CircleAvatar(
-                backgroundColor: Colors.grey,
-                radius: 100,
+              Stack(
+                alignment: Alignment.center,
+                children: <Widget>[
+                  Image(
+                    height: MediaQuery.of(context).size.height / 3,
+                    fit: BoxFit.fill,
+                    image: AssetImage('assets/cover.jpg'),
+                  )
+                ],
               ),
               SizedBox(height: 20),
               Text(
                 'Welcome to the IU Operation App',
-                style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
                 textAlign: TextAlign.center,
               ),
               SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  'This app was designed and implemented with the purpose of enhancing the tour leading experience and assessment for both the guide and the manager staffs',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16),
+                ),
+              ),
             ],
           ),
         ),
