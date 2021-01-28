ae9bdc0ac1ca91e2b22d1db10c646b0d087e6e37
Keith111216 <keithwong2205@gmail.com>
2021-01-28 11:30:32 +0700

    cleanup

--- tour_participant/chpnt_repo_manager/lib/source/firebase_chpnt_service.dart
+++ tour_participant/chpnt_repo_manager/lib/source/firebase_chpnt_service.dart
@@ -4,6 +4,7 @@ import '../chpnt_repo_manager.dart';
 
 //Class for handling checkpoint to firestore
 class FirebaseCheckpointService {
+  // ignore: deprecated_member_use
   final checkpointCollection = Firestore.instance.collection('checkpoints');
 
   //Add a checkpoint to firestore document
@@ -13,6 +14,7 @@ class FirebaseCheckpointService {
 
   //Delete a checkpoint
   Future<void> deleteCheckpoint(CheckpointModel deletedCheckpoint) async {
+    // ignore: deprecated_member_use
     return checkpointCollection.document(deletedCheckpoint.pointId).delete();
   }
 
@@ -27,6 +29,7 @@ class FirebaseCheckpointService {
       _query = checkpointCollection.snapshots();
     }
     return _query.map((snapshot) {
+      // ignore: deprecated_member_use
       return snapshot.documents
           .map((doc) =>
               CheckpointModel.fromEntity(CheckpointEntity.fromSnapshot(doc)))
@@ -37,7 +40,9 @@ class FirebaseCheckpointService {
   //Update a checkpoint details
   Future<void> updateCheckpoint(CheckpointModel updated) {
     return checkpointCollection
+        // ignore: deprecated_member_use
         .document(updated.pointId)
+        // ignore: deprecated_member_use
         .updateData(updated.toEntity().toDocument());
   }
 }
--- tour_participant/feedback_repo/lib/source/firebase_feedback_service.dart
+++ tour_participant/feedback_repo/lib/source/firebase_feedback_service.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import '../feedback_repo.dart';
 
 class FirebaseFeedbackService {
+  // ignore: deprecated_member_use
   final checkpointCollection = Firestore.instance.collection('feedbacks');
 
   //Add a checkpoint to firestore document
@@ -13,6 +14,7 @@ class FirebaseFeedbackService {
 
   //Delete a checkpoint
   Future<void> deleteCheckpoint(FeedbackModel deletedFeedback) async {
+    // ignore: deprecated_member_use
     return checkpointCollection.document(deletedFeedback.feedbackID).delete();
   }
 
@@ -20,16 +22,15 @@ class FirebaseFeedbackService {
   Stream<List<FeedbackModel>> feedbackList({String queryId}) {
     Stream<QuerySnapshot> _query;
     if (queryId != null && queryId.isNotEmpty) {
-      _query = checkpointCollection
-          .where('X', isEqualTo: queryId)
-          .snapshots();
+      _query = checkpointCollection.where('X', isEqualTo: queryId).snapshots();
     } else {
       _query = checkpointCollection.snapshots();
     }
     return _query.map((snapshot) {
+      // ignore: deprecated_member_use
       return snapshot.documents
           .map((doc) =>
-          FeedbackModel.fromEntity(FeedbackEntity.fromSnapshot(doc)))
+              FeedbackModel.fromEntity(FeedbackEntity.fromSnapshot(doc)))
           .toList();
     });
   }
@@ -37,7 +38,9 @@ class FirebaseFeedbackService {
   //Update a checkpoint details
   Future<void> updateCheckpoint(FeedbackModel updated) {
     return checkpointCollection
+        // ignore: deprecated_member_use
         .document(updated.feedbackID)
+        // ignore: deprecated_member_use
         .updateData(updated.toEntity().toDocument());
   }
 }
