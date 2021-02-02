8b664882c12ddf363d7dc6469b342cd4b280948a
Keith111216 <keithwong2205@gmail.com>
2021-02-02 21:30:27 +0700

    assets edit

--- tour_participant/lib/views/home_page.dart
+++ tour_participant/lib/views/home_page.dart
@@ -16,9 +16,56 @@ class _HomeTabNaviState extends State<HomeTabNavi> {
       appBar: welcomeAppBar(),
       drawer: MainDrawer(),
       body: Center(
-        child: RaisedButton(
-            onPressed: () =>
-                BlocProvider.of<AuthBloc>(context).add(LoggedOut())),
+        child: Container(
+          alignment: Alignment.center,
+          child: ListView(
+            children: [
+              Stack(
+                alignment: Alignment.center,
+                children: <Widget>[
+                  Image(
+                    height: MediaQuery.of(context).size.height / 3,
+                    fit: BoxFit.fill,
+                    image: AssetImage('assets/welcome.png'),
+                  )
+                ],
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Welcome to the IU Campus Tour App',
+                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Text(
+                      'This app was designed and implemented with the purpose of enhancing the tour experience for the participant of the IU Ambassador Campus Tour program',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    Text(
+                      'You can view your provided tour plan as a list of checkpoints. Create your very own profile and chat with others. We also hope that you would choose to provide your valuable feedback to the tour and the applications',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      '<<-- Get started by tapping on the drawer on the left',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
